64b084562f0bb5981c0986e70f81066dc9a64bf0
panduuuu1 <139348433+panduuuu1@users.noreply.github.com>
2025-11-25 14:34:57 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // index.js
 require("dotenv").config();
-const config = require("./config");
+const config = require("../../config");
 const { startWA, sendToWA } = require("./whatsapp");
 const startDiscord = require("./discord");
 
@@ -119,4 +119,5 @@ let discordClient = null;
     });
 
 })();
+
 
