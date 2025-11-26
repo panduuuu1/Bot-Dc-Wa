7be57d1af42690d4efeb6282af75a7862467b135
panduuuu1 <139348433+panduuuu1@users.noreply.github.com>
2025-11-26 17:33:41 +0700

    Update routes.js

---
+++ src/monitor/routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("panel");
+});
+
+router.get("/qr", (req, res) => {
+    res.render("qr");
+});
+
+module.exports = router;
